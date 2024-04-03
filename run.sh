feef90975b0cc4f94b882c86b13857dba7b3da2e
edsuu <111332935+edsuuu@users.noreply.github.com>
2024-04-03 02:08:42 -0300

    Ajustes

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java -Xmx28G -Xms28G @libraries/net/minecraftforge/forge/1.20.1-47.2.0/unix_args.txt "$@"
+# java -Xmx30G -Xms30G @libraries/net/minecraftforge/forge/1.20.1-47.2.0/unix_args.txt "$@"
+java -Xmx30G -Xms30G -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=8 @libraries/net/minecraftforge/forge/1.20.1-47.2.0/unix_args.txt "$@"
