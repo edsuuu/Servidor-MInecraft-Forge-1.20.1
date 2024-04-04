1bebb09cce45fcead6feff37180918b20aa0b6cb
root <root@servidor-minecraft.southamerica-east1-b.c.mineral-subject-416019.internal>
2024-04-04 06:44:39 +0000

    Backup Diario 04-04-2024

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
 # java -Xmx30G -Xms30G @libraries/net/minecraftforge/forge/1.20.1-47.2.0/unix_args.txt "$@"
-java -Xmx30G -Xms30G -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=8 @libraries/net/minecraftforge/forge/1.20.1-47.2.0/unix_args.txt "$@"
+java -Xmx30G -Xms30G @libraries/net/minecraftforge/forge/1.20.1-47.2.0/unix_args.txt "$@"
